f84db392db4fd68e9dfbfe8d668375c834b15ebe
brunovgois <brunovgois@gmail.com>
2023-01-10 23:19:54 -0300

    layout improvements

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,35 +1,50 @@
 import styled from "styled-components";
+import { pokeTypeColors } from "../utils/type_colors";
 
 export default function Pokemon({ data }) {
+
+  const pokeTypes = data.type.map((type, idx) => {
+    return (
+      <PokeTypeContainer bgColor={pokeTypeColors[data.type[idx]]}>
+        <PokeTypeDesc>{type}</PokeTypeDesc>
+      </PokeTypeContainer>
+    );
+  })
+
   return (
     <PokeListItem>
       <ImageContainer>
-        <img src={data.img} alt="pokemon profile picture" />
+        <Image src={data.img} alt="pokemon profile picture" />
       </ImageContainer>
       <PokeNumberWrapper>
         <PokeNumber>#{data.num}</PokeNumber>
       </PokeNumberWrapper>
       <Name>{data.name}</Name>
 
-      {/* <p>TODO: type</p> */}
+      <TypeContainer>{pokeTypes}</TypeContainer>
     </PokeListItem>
   );
 }
 
-const PokeListItem = styled.li``;
+const PokeListItem = styled.li`
+  margin: 12px`;
 
 const ImageContainer = styled.div`
   background-color: #d1d5db;
-  display: flex;
-  justify-content: center;
+  max-width: 300px
 `;
-const PokeNumberWrapper = styled.div`
-background-color: #f9fafb;
-width: 50%;
-border-radius: 0 10px 10px 0;
-margin-top: -3px;
-margin-rigth: 25px
+const Image = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 `
+const PokeNumberWrapper = styled.div`
+  background-color: #f9fafb;
+  width: 50%;
+  border-radius: 0 10px 10px 0;
+  margin-top: -3px;
+  margin-rigth: 25px;
+`;
 const PokeNumber = styled.p`
   font-size: 10px;
   margin: 0;
@@ -38,4 +53,23 @@ const PokeNumber = styled.p`
 const Name = styled.p`
   margin: 0;
   font-weight: 600;
+  margin-bottom: 2px
+`;
+const TypeContainer = styled.div `
+  display: flex;
+  flex-direction: row;
+  gap: 2px
 `
+const PokeTypeContainer = styled.div`
+  border-radius: 5px;
+  background-color: ${(props) => props.bgColor};
+  width: 30%;
+  display: flex;
+  justify-content: center;
+  
+`;
+
+const PokeTypeDesc = styled.p`
+  margin: 0;
+  font-size: 10px;
+`;
---
+++ src/utils/type_colors.js
@@ -0,0 +1,19 @@
+export const pokeTypeColors = {
+  Bug: "#A6B91A",
+  Rock: "#B6A136",
+  Ghost: "#735797",
+  Dragon: "#6F35FC",
+  Psychic: "#F95587",
+  Normal: "#A8A77A",
+  Fire: "#EE8130",
+  Water: "#6390F0",
+  Electric: "#F7D02C",
+  Grass: "#7AC74C",
+  Ice: "#96D9D6",
+  Fighting: "#C22E28",
+  Poison: "#A33EA1",
+  Ground: "#E2BF65",
+  Flying: "#A98FF3",
+};
+
+export const whiteColorBgText = [Ghost, Dragon, Fighting, Poison];
